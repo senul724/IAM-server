d987d05e87a5552cf271ac5ec5ae6b1b900babd1
senul724 <hardlove.mrplus@gmail.com>
2026-02-10 00:38:12 +0530

    feat(utils): created jwt helper file

---
+++ server/src/utils/jwt.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"IAM-server/src/types"
+	"encoding/json"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+const key = "yqY9OPPUy4RouGWbelqwUwlxqyu9NwzFMZNrZJlcfLV"
+
+func CreateToken(userId string, userdata *types.UserData) (string, error) {
+	jsonData, err := json.Marshal(userdata)
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"jti":  uuid.New(),
+		"sub":  userId,
+		"user": jsonData,
+		"exp":  time.Now().Add(time.Minute).Unix(),
+	})
+
+	return token.SignedString([]byte(key))
+}
