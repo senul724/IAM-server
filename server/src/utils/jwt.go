74eef39aa7a79684d1cbd462767ee65d12b891a3
senul724 <hardlove.mrplus@gmail.com>
2026-02-10 22:08:21 +0530

    feat(token): access token retrieval and validation added

--- server/src/utils/jwt.go
+++ server/src/utils/jwt.go
@@ -4,6 +4,7 @@ import (
 	"IAM-server/src/types"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -11,57 +12,60 @@ import (
 )
 
 const (
-	Key            = "yqY9OPPUy4RouGWbelqwUwlxqyu9NwzFMZNrZJlcfLV"
-	expirationTime = time.Hour * 24 * 14 // 14 days
+	Key        = "yqY9OPPUy4RouGWbelqwUwlxqyu9NwzFMZNrZJlcfLV"
+	refreshExp = time.Hour * 24 * 14 // 14 days
+	accessExp  = time.Minute * 5     // 14 days
 )
 
 func CreateRefreshToken(userId string, userdata types.UserData) (string, error) {
+	return createJwt(userId, userdata, refreshExp, Key)
+}
+
+func CreateAccessToken(userId string, userdata types.UserData) (string, error) {
+	return createJwt(userId, userdata, accessExp, Key)
+}
+
+func createJwt(userId string, userdata types.UserData, exp time.Duration, key string) (string, error) {
 	claims := types.CustomClaims{
 		User: userdata,
 		RegisteredClaims: jwt.RegisteredClaims{
 			Issuer:    "http://localhost:8000",
 			Subject:   userId,
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationTime)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(exp)),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			ID:        uuid.New().String(),
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(Key))
+	return token.SignedString([]byte(key))
 }
 
-func CheckRefreshToken(r *http.Request) error {
+func VerifyRefreshToken(r *http.Request) error {
 	cookie, cookieErr := r.Cookie("iam-refresh")
 	if cookieErr != nil {
 		return cookieErr
 	}
-	_, validationError := validateTokenWithClaims(cookie.Value, Key)
+	_, validationError := validateToken(cookie.Value, Key)
 	return validationError
 }
 
-func validateToken(tokenString string, secretKey string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Validate the signing method
-		_, ok := token.Method.(*jwt.SigningMethodHMAC)
-		if !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(secretKey), nil
-	})
-
-	if err != nil {
-		return nil, err
+func VerifyAccessToken(r *http.Request) (*types.CustomClaims, error) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return nil, fmt.Errorf("Authorization header not present")
 	}
 
-	if !token.Valid {
-		return nil, fmt.Errorf("invalid token")
+	const prefix = "Bearer "
+	if !strings.HasPrefix(authHeader, prefix) {
+		return nil, fmt.Errorf("Invalid Authorization Header")
 	}
 
-	return token, nil
+	tokenString := strings.TrimSpace(strings.TrimPrefix(authHeader, prefix))
+	return validateToken(tokenString, Key)
 }
 
-func validateTokenWithClaims(tokenString string, secretKey string) (*types.CustomClaims, error) {
+func validateToken(tokenString string, secretKey string) (*types.CustomClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &types.CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		// Validate signing method
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
