72c36f7a2a95bade5de47febfc4aa29c94ca41b9
senul724 <hardlove.mrplus@gmail.com>
2026-02-10 14:39:45 +0530

    feat(jwt): added utilities to validate and decode jwt

--- server/src/types/types.go
+++ server/src/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "github.com/golang-jwt/jwt/v5"
+
 type UserCredential struct {
 	Site  string `json:"site"`
 	Email string `json:"email"`
@@ -17,4 +19,12 @@ type UserData struct {
 	Name     string `json:"name"`
 	PhotoUrl string `json:"photo_url"`
 	Email    string `json:"email"`
+}
+
+type CustomClaims struct {
+	UserID   int      `json:"user_id"`
+	Username string   `json:"username"`
+	Role     string   `json:"role"`
+	User     UserData `json:"user"`
+	jwt.RegisteredClaims
 }
--- server/src/utils/jwt.go
+++ server/src/utils/jwt.go
@@ -2,27 +2,81 @@ package utils
 
 import (
 	"IAM-server/src/types"
-	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
-const key = "yqY9OPPUy4RouGWbelqwUwlxqyu9NwzFMZNrZJlcfLV"
+const (
+	Key            = "yqY9OPPUy4RouGWbelqwUwlxqyu9NwzFMZNrZJlcfLV"
+	expirationTime = time.Hour * 24 * 14 // 14 days
+)
 
-func CreateRefreshToken(userId string, userdata *types.UserData) (string, error) {
-	jsonData, err := json.Marshal(userdata)
-	if err != nil {
-		return "", err
+func CreateRefreshToken(userId string, userdata types.UserData) (string, error) {
+	claims := types.CustomClaims{
+		User: userdata,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "http://localhost:8000",
+			Subject:   userId,
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationTime)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        uuid.New().String(),
+		},
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"jti":  uuid.New(),
-		"sub":  userId,
-		"user": jsonData,
-		"exp":  time.Now().Add(time.Minute).Unix(),
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(Key))
+}
+
+func CheckRefreshToken(r *http.Request) error {
+	cookie, cookieErr := r.Cookie("iam-refresh")
+	if cookieErr != nil {
+		return cookieErr
+	}
+	_, validationError := validateTokenWithClaims(cookie.Value, Key)
+	return validationError
+}
+
+func validateToken(tokenString string, secretKey string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the signing method
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
 	})
 
-	return token.SignedString([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return token, nil
+}
+
+func validateTokenWithClaims(tokenString string, secretKey string) (*types.CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &types.CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// Validate signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*types.CustomClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token claims")
 }
