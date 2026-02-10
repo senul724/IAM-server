6bddad8db74c848fec5b46d34deb028f8c411b0e
senul724 <hardlove.mrplus@gmail.com>
2026-02-10 23:00:41 +0530

    feat(token): created handler for access token issuing

--- server/src/handlers/tokens/issue-access.go
+++ server/src/handlers/tokens/issue-access.go
@@ -2,18 +2,29 @@ package tokens
 
 import (
 	"IAM-server/src/utils"
+	"encoding/json"
 	"net/http"
 )
 
 func IssueAccess(w http.ResponseWriter, r *http.Request) {
 	// check for refresh token
-	cookieErr := utils.VerifyRefreshToken(r)
+	claims, cookieErr := utils.VerifyRefreshToken(r)
 	if cookieErr != nil {
 		http.Error(w, cookieErr.Error(), http.StatusBadRequest)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("access token"))
+	// creating the access token
+	token, tokenError := utils.CreateAccessToken(claims.Subject, claims.User)
+	if tokenError != nil {
+		http.Error(w, tokenError.Error(), http.StatusBadRequest)
+		return
+	}
 
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": token,
+	})
 }
